69fea7eaf62ccb71191cd98855283251a17dfe9f
EzraJRogers <applebo@applebos-MacBook-Air.local>
2024-04-08 21:54:17 -0700

    Set up handlebars and routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,20 +1,19 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User,Event } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+    const eventData = await User.findAll({
+      include: [
+        {
+          model: Event,
+        },
+      ],
     });
+    const events = eventData.map((data)=> data.get({plain: true}));
 
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('homepage', {
-      users,
-      logged_in: req.session.logged_in,
-    });
+    res.render('home', {events, logged_in: req.session.logged_in});
   } catch (err) {
     res.status(500).json(err);
   }
@@ -28,5 +27,19 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
+
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
+router.get('/form', (req, res) => {
+  res.render('form');
+});
+
 
 module.exports = router;
