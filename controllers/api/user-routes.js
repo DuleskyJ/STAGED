232292d4dc3ba07311c3b8d7211105aa586c27b8
EzraJRogers <applebo@applebos-Air.MG8702>
2024-04-10 16:58:44 -0700

    fixed user auth

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -14,18 +14,10 @@ router.get('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({ where: { email: req.body.email, password: req.body.password } });
+    console.log(userData)
 
     if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
       res
         .status(400)
         .json({ message: 'Incorrect email or password, please try again' });
